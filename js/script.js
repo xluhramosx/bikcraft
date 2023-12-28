38b593f0828e88abe4187a6d04945de63b73eb22
Luciana Ramos <ramos.d.luciana@gmail.com>
2023-12-28 00:01:22 -0300

    Implementando JavaScript em Perguntas Frequentes

--- js/script.js
+++ js/script.js
@@ -26,5 +26,22 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto);
 
-console.log(parametros);
+//Parguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', 'true');
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
 
